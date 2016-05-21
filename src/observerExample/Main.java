699de24f193176b38b597fdec4e8bda0746a505f
RegularSvensson <elias.svensson.1992@gmail.com>
2016-05-21 13:10:15 +0200

    Add project files

---
+++ src/observerExample/Main.java
@@ -0,0 +1,20 @@
+package observerExample;
+
+ 
+
+import observerExample.view.*;
+import observerExample.model.*;
+
+/**
+ * This class contains the data handling part of the program
+ * 
+ * @author Uno Holmer 
+ * @version 2007-05-08
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+        new UserInterface( new PrimeGenerator(), new FibonacciGenerator() );
+	}
+	
+}
